720ae044def68ea174902ac986e01707e48a7b0b
Dar-Suvorov <suvorov.2016@yandex.ru>
2024-05-06 12:09:19 +0300

    (index) Добавляет слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,27 +1,9 @@
-// const sliderToggle = document.querySelector('.slider__toggle');
-// const slider = document.querySelector('.slider');
-
-// if (slider) {
-//   let flag = false;
-//   sliderToggle.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     if (flag) {
-//       slider.classList.toggle('slider--after');
-//       slider.classList.toggle('slider--before');
-//     } else {
-//       flag = true;
-//       slider.classList.add('slider--after');
-//     }
-//   });
-// }
-
-
+// Меню навигации в шапке.
 const navMenu = document.querySelector('.header-nav');
 const navButton = document.querySelector('.header-nav__button-menu');
 const main = document.querySelector('main');
 
 navMenu.classList.remove('header-nav--no-js');
-// navMenu.classList.remove('header-nav--opened');
 navMenu.classList.add('header-nav--closed');
 
 navButton.addEventListener('click', () => {
@@ -40,3 +22,21 @@ main.addEventListener('click', () => {
     navMenu.classList.add('header-nav--closed');
   }
 });
+
+// Слайдер на странице Index.
+const sliderToggle = document.querySelector('.slider__toggle');
+const slider = document.querySelector('.slider');
+
+if (slider) {
+  let flag = false;
+  sliderToggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (flag) {
+      slider.classList.toggle('slider--before');
+      slider.classList.toggle('slider--after');
+    } else {
+      flag = true;
+      slider.classList.add('slider--before');
+    }
+  });
+}
